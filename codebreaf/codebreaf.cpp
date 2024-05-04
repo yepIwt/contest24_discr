fc5ed34619f30ab30208104a2c89493beae29ed3
yepIwt <ReetRay@protonmail.com>
2024-05-04 18:14:46 +0300

    C task done

--- codebreaf/codebreaf.cpp
+++ codebreaf/codebreaf.cpp
@@ -1,58 +1,44 @@
 ﻿#include <iostream>
-#include <algorithm>
 #include <vector>
 #include <fstream>
 #include <string>
 
-// https://informatics.msk.ru/mod/statements/view.php?id=211&chapterid=85#1
+// https://informatics.msk.ru/mod/statements/view.php?id=211&chapterid=89#1
 
-
-int generate_n() {
-    std::srand((unsigned)std::time(0));
-    return (rand() % 10) + 1;
+void recursion(int summa, std::vector<int> prefix, std::ofstream& file) {
+    if (summa == 0) {
+        for (int i : prefix) {
+            file << i << " ";
+        }
+        file << std::endl;
+    }
+    else {
+        for (int i = 1; i <= summa; i++) {
+            if (prefix.empty() || prefix.back() >= i) {
+                prefix.push_back(i);
+                recursion(summa - i, prefix, file);
+                prefix.pop_back();
+            }
+        }
+    }
 }
 
-
 int main() {
+    for (int summa = 1; summa <= 40; summa++) {
 
-    int n;
-
-    for (int i = 0; i < 10; i++) {
-
-        n = i;
-        int test_case = i + 1;
-
-        std::ofstream input_file;
-        std::string filename = "tests\\";
-        if (test_case < 10) filename += "0";
-        std::string input_path = filename + std::to_string(test_case);
-        input_file.open(input_path.c_str());
-        input_file << n;
-        input_file.close();
-
-
-        std::ofstream output_file;
-        std::string output_path = input_path + ".a";
-        output_file.open(output_path.c_str());
-
-
-        std::vector<int> numbers(n);
-        for (int i = 0; i < n; ++i) {
-            numbers[i] = i + 1;
+        std::string filename = "tests/";
+        if (summa < 10) {
+            filename += "0";
         }
+        filename += std::to_string(summa);
 
-        for (int num : numbers) {
-            output_file << num;
-        }
-        output_file << std::endl;
+        std::ofstream inputFile(filename);
+        inputFile << summa;
+        inputFile.close();
 
-        while (std::next_permutation(numbers.begin(), numbers.end())) {
-            for (int num : numbers) {
-                output_file << num;
-            }
-            output_file << std::endl;
-        }
-        output_file.close();
+        std::ofstream outputFile(filename + ".a");
+        recursion(summa, {}, outputFile);
+        outputFile.close();
     }
 
     return 0;
